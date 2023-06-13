e7bdc1b55a5b2f3a7c69f27d7d0b9ff5acf582cb
edempixels <info@edempixels.com>
2023-06-12 17:04:11 -0700

    PLIIR

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -4,5 +4,7 @@
 void print_list_integer(int *my_list, size_t size);
 int element_at(int *my_list, int idx);
 void replace_in_list(int *my_list, int idx, int element);
+void print_reversed_list_integer(int *my_list, size_t size);
+
 
 #endif /* LISTS_H */
